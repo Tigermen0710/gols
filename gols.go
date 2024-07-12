3300a4f6405485136ea1d1bec5f282c3c6377e18
elbachir-one <bachiralfa@gmail.com>
2024-07-12 16:04:20 +0100

    More code imporvement

--- gols.go
+++ gols.go
@@ -8,19 +8,68 @@ import (
 	"strings"
 )
 
-var reset = "\033[0m"
-var red = "\033[31m"
-var green = "\033[32m"
-var yellow = "\033[33m"
-var blue = "\033[34m"
-var magenta = "\033[35m"
-var cyan = "\033[36m"
-var white = "\033[97m"
-var orange = "\033[38;5;208m"
-var purple = "\033[35m"
+var (
+	reset   = "\033[0m"
+	red     = "\033[31m"
+	green   = "\033[32m"
+	yellow  = "\033[33m"
+	blue    = "\033[34m"
+	magenta = "\033[35m"
+	cyan    = "\033[36m"
+	white   = "\033[97m"
+	orange  = "\033[38;5;208m"
+	purple  = "\033[35m"
+)
 
-const maxFilesInLine = 3
-const maxFileNameLength = 19
+const (
+	maxFilesInLine     = 3
+	maxFileNameLength  = 19
+)
+
+var fileIcons = map[string]string{
+	".go":   cyan + " " + reset,
+	".sh":   white + " " + reset,
+	".cpp":  blue + " " + reset,
+	".hpp":  blue + " " + reset,
+	".cxx":  blue + " " + reset,
+	".hxx":  blue + " " + reset,
+	".css":  blue + " " + reset,
+	".c":    blue + " " + reset,
+	".png":  magenta + " " + reset,
+	".jpg":  magenta + " " + reset,
+	".jpeg": magenta + " " + reset,
+	".webp": magenta + " " + reset,
+	".xcf":  white + " " + reset,
+	".xml":  red + " " + reset,
+	".htm":  red + " " + reset,
+	".html": red + " " + reset,
+	".txt":  white + " " + reset,
+	".mp3":  cyan + " " + reset,
+	".ogg":  cyan + " " + reset,
+	".zip":  yellow + "󰿺 " + reset,
+	".tar":  yellow + "󰿺 " + reset,
+	".gz":   yellow + "󰿺 " + reset,
+	".bz2":  yellow + "󰿺 " + reset,
+	".xz":   yellow + "󰿺 " + reset,
+	".jar":  white + " " + reset,
+	".java": white + " " + reset,
+	".js":   yellow + " " + reset,
+	".py":   yellow + " " + reset,
+	".rs":   orange + " " + reset,
+	".deb":  red + " " + reset,
+	".md":   blue + " " + reset,
+	".rb":   red + " " + reset,
+	".php":  purple + " " + reset,
+	".pl":   orange + " " + reset,
+	".svg":  magenta + " " + reset,
+	".eps":  magenta + " " + reset,
+	".ps":   magenta + " " + reset,
+	".git":  orange + " " + reset,
+}
+
+var binaryExtensions = map[string]bool{
+	".exe": true, ".bin": true, ".o": true, ".so": true, ".dll": true, ".out": true,
+}
 
 func main() {
 	directory, err := os.Getwd()
@@ -49,6 +98,7 @@ func main() {
 func printFile(file os.DirEntry) {
 	name := file.Name()
 	ext := strings.ToLower(filepath.Ext(name))
+	icon, exists := fileIcons[ext]
 
 	if ext == "" {
 		if file.IsDir() {
@@ -56,59 +106,12 @@ func printFile(file os.DirEntry) {
 		} else {
 			fmt.Print(green + " " + reset + name)
 		}
+	} else if exists {
+		fmt.Print(icon + name)
+	} else if isBinary(ext) {
+		fmt.Print(green + " " + reset + name)
 	} else {
-		switch ext {
-		case ".go":
-			fmt.Print(cyan + " " + reset + name)
-		case ".sh":
-			fmt.Print(white + " " + reset + name)
-		case ".cpp", ".hpp", ".cxx", ".hxx":
-			fmt.Print(blue + " " + reset + name)
-		case ".css":
-			fmt.Print(blue + " " + reset + name)
-		case ".c":
-			fmt.Print(blue + " " + reset + name)
-		case ".png", ".jpg", ".jpeg", ".webp":
-			fmt.Print(magenta + " " + reset + name)
-		case ".xcf":
-			fmt.Print(white + " " + reset + name)
-		case ".xml", ".htm", ".html":
-			fmt.Print(red + " " + reset + name)
-		case ".txt":
-			fmt.Print(white + " " + reset + name)
-		case ".mp3", ".ogg":
-			fmt.Print(cyan + " " + reset + name)
-		case ".zip", ".tar", ".gz", ".bz2", ".xz":
-			fmt.Print(yellow + "󰿺 " + reset + name)
-		case ".jar", ".java":
-			fmt.Print(white + " " + reset + name)
-		case ".js":
-			fmt.Print(yellow + " " + reset + name)
-		case ".py":
-			fmt.Print(yellow + " " + reset + name)
-		case ".rs":
-			fmt.Print(orange + " " + reset + name)
-		case ".deb":
-			fmt.Print(red + " " + reset + name)
-		case ".md":
-			fmt.Print(blue + " " + reset + name)
-		case ".rb":
-			fmt.Print(red + " " + reset + name)
-		case ".php":
-			fmt.Print(purple + " " + reset + name)
-		case ".pl":
-			fmt.Print(orange + " " + reset + name)
-		case ".svg", ".eps", ".ps":
-			fmt.Print(magenta + " " + reset + name)
-		case ".git":
-			fmt.Print(orange + " " + reset + name)
-		default:
-			if isBinary(file) {
-				fmt.Print(green + " " + reset + name)
-			} else {
-				fmt.Print(white + " " + reset + name)
-			}
-		}
+		fmt.Print(white + " " + reset + name)
 	}
 }
 
@@ -119,13 +122,6 @@ func printPadding(fileName string) {
 	}
 }
 
-func isBinary(file os.DirEntry) bool {
-	binaryExtensions := []string{".exe", ".bin", ".o", ".so", ".dll", ".out"}
-	ext := strings.ToLower(filepath.Ext(file.Name()))
-	for _, binExt := range binaryExtensions {
-		if ext == binExt {
-			return true
-		}
-	}
-	return false
+func isBinary(ext string) bool {
+	return binaryExtensions[ext]
 }
